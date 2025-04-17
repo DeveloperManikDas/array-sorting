f6772807b5cdb78c0c07c5f38dcb6e1f4e872ee7
DeveloperManikDas <developermanikdas@gmail.com>
2025-04-17 22:36:56 +0530

    add insertion sort

--- arraySorting.java
+++ arraySorting.java
@@ -18,9 +18,51 @@ class arraySorting {
         }
     }
 
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+
+        int smallest = arr[0];
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                if (arr[j] < smallest) {
+                    smallest = arr[j];
+                }
+            }
+        }
+        System.out.print("Sorted array is ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    // Function to perform insertion sort
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; // element to be inserted
+            int j = i - 1;
+
+            // Shift elements of arr[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+        }
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
     public static void main(String arg[]) {
         int arr[] = { 3, 1, 5, 4, 2 };
-        bubbleSort(arr);
-        
+        // bubbleSort(arr);
+        // selectionSort(arr);
+        insertionSort(arr);
+
     }
 }
