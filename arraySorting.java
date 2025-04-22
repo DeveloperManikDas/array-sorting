bc73ccf49e19ba3b10bfdfdf80028bb66cd4ff74
DeveloperManikDas <developermanikdas@gmail.com>
2025-04-22 23:37:20 +0530

    print sprial num in 2D array

--- arraySorting.java
+++ arraySorting.java
@@ -134,14 +134,59 @@ public class arraySorting {
         }
     }
 
+    public static void printSpiral(int[][] matrix) {
+        if (matrix.length == 0)
+            return;
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            // Print top row
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            // Print right column
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            // Print bottom row if not already printed
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            // Print left column if not already printed
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 3, 0, 1, 5, 4, 2 };
+        int[][] arr = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8}
+        };
 
         // Uncomment one of the sorting methods to test
         // bubbleSort(arr);
         // selectionSort(arr);
         // insertionSort(arr);
         // countingSort(arr);
-        moveZeroes(arr);
+        // moveZeroes(arr);
+        printSpiral(arr);
     }
 }
